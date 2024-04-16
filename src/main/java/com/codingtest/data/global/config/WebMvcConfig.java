86a46da937b49a5b2506bae3f7265dd342b55d1a
wpghks <wpghks56@naver.com>
2024-04-16 14:40:07 +0900

    [Feature] Cors 충돌 해결

--- src/main/java/com/codingtest/data/global/config/WebMvcConfig.java
+++ src/main/java/com/codingtest/data/global/config/WebMvcConfig.java
@@ -1,7 +1,12 @@
 package com.codingtest.data.global.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,11 +20,30 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**") //CORS 적용할 URL 패턴
-                .allowedOriginPatterns("*") //자원 공유 오리진 지정
-                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS") //요청 허용 메서드
-                .allowedHeaders("*") //요청 허용 헤더
+                .allowedOriginPatterns(CorsConfiguration.ALL) //자원 공유 오리진 지정
+                .allowedMethods(CorsConfiguration.ALL) //요청 허용 메서드
+                .allowedHeaders(CorsConfiguration.ALL) //요청 허용 헤더
                 .allowCredentials(true) //요청 허용 쿠키
-                .exposedHeaders("X-User-Information")
+                .exposedHeaders("X-From")
                 .maxAge(MAX_AGE_SECS);
     }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(6000L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));
+        filterBean.setOrder(0);
+
+        return filterBean;
+    }
+
 }
